3c250fadf3ed7f8b8cb2bd0627b0236d2cb71833
Mateusz Nurzyński <mateonurzynski@gmail.com>
2023-07-10 17:27:03 +0200

    Feat: Implement capitalize() to capitalize the first letter of a string

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const capitalize = (string) => {
+  if (typeof string !== 'string') {
+    return false;
+  }
+  const firstLetter = string.charAt(0);
+  const arrayString = string.split('');
+  arrayString[0] = firstLetter.toUpperCase();
+  return arrayString.join('');
+};
 
+export { capitalize };
---
+++ index.test.js
@@ -0,0 +1,17 @@
+import { capitalize } from '.';
+
+test('should capitalize the first letter', () => {
+  expect(capitalize('string')).toBe('String');
+});
+
+test('should leave the string as it is when already capitalized', () => {
+  expect(capitalize('String')).toBe('String');
+});
+
+test('should leave the string as it is when empty', () => {
+  expect(capitalize('')).toBe('');
+});
+
+test('should return false when passing other data types', () => {
+  expect(capitalize(123)).toBe(false);
+});
